27e5f1c74d7ec50d9cefc3f8df7f87559cf67741
noelia <noeliarolli82@gmail.com>
2021-06-12 20:50:43 -0300

    prueba commit

---
+++ Unidad3/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+for( var i=0; i<3; i++){
+
+    var nombre = prompt("Ingrese su nombre")
+    var edad = Number(prompt("Ingrese su edad"));
+
+    if (edad >= 18){
+
+        alert( nombre + ' es mayor de edad ') 
+
+    }else{
+
+        alert( nombre + ' es menor de edad ')
+
+    }
+    
+}
+
+
+
